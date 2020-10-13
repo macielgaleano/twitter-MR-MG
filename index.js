c8cf6d38da21e9387a7defc98e850f7482949087
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-10-13 11:57:50 -0300

    Routes defined

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 3000;
+const mongoose = require("mongoose");
+const { routes } = require("./routes");
 
-app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Example app listening on port port!`));
+// Configuracion app
+app.use(express.static("public"));
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs", "formidable");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+routes(app);
+
+app.listen(process.env.APP_PORT, () =>
+  console.log(`App on: http://localhost:${process.env.APP_PORT}`)
+);
---
+++ routes.js
@@ -0,0 +1,7 @@
+const routes = (app) => {
+  app.get("/", (req, res) => res.send("Hello World!"));
+};
+
+module.exports = {
+  routes,
+};
