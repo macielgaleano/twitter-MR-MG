107818468a6081425ec3d6fd3409f173d30af8b2
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-10-13 14:05:37 -0300

    Routes created

--- routes.js
+++ routes.js
@@ -1,7 +1,19 @@
 const tweetController = require("./controllers/tweet.controller");
 
 const routes = (app) => {
+  //Home page
   app.get("/", tweetController.allTweets);
+
+  //Login, register pages
+
+  app.get("/login", (req, res) => {});
+  app.get("/register", (req, res) => {});
+  app.post("/login", (req, res) => {});
+  app.post("/register", (req, res) => {});
+
+  //Profile page
+
+  app.post("/:username", (req, res) => {});
 };
 
 module.exports = {
