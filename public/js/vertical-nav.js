613c8c4721659bc00aec9281949dc19ca6d54ff1
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-10-14 19:44:44 -0300

    Fixed vertical nav

--- public/js/vertical-nav.js
+++ public/js/vertical-nav.js
@@ -2,11 +2,11 @@ $(function () {
   // Sidebar toggle behavior
   $("#sidebar").hide();
   $(".sidebarCollapseMenu").on("click", function () {
-    $("#sidebar, #content").slideDown();
+    $("#sidebar, #content").show("slide", { direction: "right" }, 1000);
     $(".sidebarCollapseMenu").hide();
   });
   $("#sidebarCollapse").on("click", function () {
-    $("#sidebar, #content").hide();
+    $("#sidebar, #content").hide("slide", { direction: "right" }, 1000);
     $(".sidebarCollapseMenu").show();
   });
 });
