4d0c40ba7ddaa1a459febe1a9c531d17519bfb1f
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-10-15 16:23:38 -0300

    Fixed errors for pull

--- routes.js
+++ routes.js
@@ -3,16 +3,9 @@ const tweetController = require("./controllers/tweet.controller");
 const seeder = require("./seeder");
 
 const routes = (app) => {
-<<<<<<< HEAD
-
-  //Home page
-  app.get("/welcome", userController.welcome)
-  app.get("/", tweetController.allTweets);
-  
-=======
   //Home page
   app.get("/welcome", userController.welcome);
->>>>>>> c262da080ea93c9c218064d2b839b90b552132a5
+
   //Login, register pages
   app.get("/usuario/:username", isLoggedIn, userController.userPage);
   app.get("/login-registro", userController.showLoginRegistro);
@@ -28,11 +21,10 @@ const routes = (app) => {
   app.post("/tweet/crear", tweetController.createTweets);
   app.post("/registro", userController.createUser);
   app.post("/login", userController.login);
-<<<<<<< HEAD
 
   app.get("/auth/facebook/callback", userController.facebookLogin);
   app.get("/auth/facebook", userController.facebookAuth);
-  
+
   app.get("/home/:id", isLoggedIn, tweetController.home);
   app.use("/", isLoggedIn, tweetController.homeFirst);
   //Profile page
@@ -42,10 +34,6 @@ const routes = (app) => {
   app.get("/usuario/:tweetId/borrar", isLoggedIn, tweetController.delete);
   app.get("/creardata", seeder.createTweets);
 
-=======
-  app.get("/auth/facebook/callback", userController.facebookLogin);
-  app.get("/auth/facebook", userController.facebookAuth);
->>>>>>> c262da080ea93c9c218064d2b839b90b552132a5
   app.get("/logout", (req, res) => {
     req.logout();
     res.redirect("/login-register");
