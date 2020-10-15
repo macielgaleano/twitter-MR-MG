04b76259be276f4aeab20f60843f51c8de0d4fb2
Mikaela22 <61974476+Mikaela22@users.noreply.github.com>
2020-10-15 16:19:58 -0300

    changes

--- routes.js
+++ routes.js
@@ -3,29 +3,23 @@ const tweetController = require("./controllers/tweet.controller");
 const seeder = require("./seeder");
 
 const routes = (app) => {
-<<<<<<< HEAD
+
   //Home page
   app.get("/welcome", userController.welcome)
   app.get("/", tweetController.allTweets);
   
-
-=======
->>>>>>> d71653ae4e07c9e4a5ab49bae9cb4e11ea320663
   //Login, register pages
 
   app.get("/login-registro", userController.showLoginRegistro);
 
   app.post("/registro", userController.createUser);
   app.post("/login", userController.login);
-<<<<<<< HEAD
-  app.use("/", isLoggedIn);
+
   app.get("/auth/facebook/callback", userController.facebookLogin);
   app.get("/auth/facebook", userController.facebookAuth);
   
-=======
   app.get("/home/:id", isLoggedIn, tweetController.home);
   app.use("/", isLoggedIn, tweetController.homeFirst);
->>>>>>> d71653ae4e07c9e4a5ab49bae9cb4e11ea320663
   //Profile page
   app.get("/usuario/:username", isLoggedIn, userController.userPage);
   app.get("/usuario/:username/like", isLoggedIn, userController.like);
