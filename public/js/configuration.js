82b7fed0bca6b4724c7c25d1b8f0aa5ce9f9d30a
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-10-18 11:52:36 -0300

    VIEW: configuration view ended
    Next step are make the functionality for uplad the information

--- public/js/configuration.js
+++ public/js/configuration.js
@@ -14,3 +14,18 @@ $(".file-drop-zone-title").text("Arrastre sus archivos aqui!!");
 $(".file-caption-name").attr("placeholder", "Seleccione sus archivos");
 $(".btn").addClass("btn-danger");
 $(".hidden-xs").text("Buscar");
+
+$(document).ready(function () {
+  $("#show_hide_password a").on("click", function (event) {
+    event.preventDefault();
+    if ($("#show_hide_password input").attr("type") == "text") {
+      $("#show_hide_password input").attr("type", "password");
+      $("#show_hide_password i").addClass("fa-eye-slash");
+      $("#show_hide_password i").removeClass("fa-eye");
+    } else if ($("#show_hide_password input").attr("type") == "password") {
+      $("#show_hide_password input").attr("type", "text");
+      $("#show_hide_password i").removeClass("fa-eye-slash");
+      $("#show_hide_password i").addClass("fa-eye");
+    }
+  });
+});
